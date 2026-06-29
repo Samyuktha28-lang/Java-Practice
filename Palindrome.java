72e0b02b927506f2d4f89f121bd5878dff3f3ebd
Samyuktha28-lang <samyukram28@gmail.com>
2026-06-29 21:29:37 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+	  public static void main(String[]args){
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter a String: ");
+	    String str=sc.nextLine();
+	    char[]ch=str.toCharArray();
+	    
+	    int a=0;
+	    int b=ch.length-1;
+	    boolean flag=true;
+	    
+	    while(b>a){
+	      if(ch[b]!=ch[a]){
+	    	  
+	        flag =false;
+	        break;
+	      }
+	      a++;
+	      b--;
+	    }
+	    if(flag){
+	      System.out.println("YES");
+	    }
+	    else{
+	       System.out.println("NO");
+	    }sc.close();
+	 }
+}
+      
